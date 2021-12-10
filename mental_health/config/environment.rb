c5c3918573957641ea81eb39b252cf2f9186fc15
kvantonio <kvantonio@i.ua>
2021-12-10 17:27:16 +0200

    mailer settings (mailtrap)

--- mental_health/config/environment.rb
+++ mental_health/config/environment.rb
@@ -5,17 +5,16 @@ require_relative "application"
 Rails.application.initialize!
 
 Rails.application.configure do
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV['MAIL_ADDRESS'],
-    password:             ENV['MAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :user_name => ENV['MAIL_LOGIN'],
+    :password => ENV['MAIL_PASSWORD'],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
   }
 
 end
