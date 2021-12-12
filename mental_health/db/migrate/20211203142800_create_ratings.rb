ec19d441db53e064a62550ef7c28d06d3be20b06
kvantonio <kvantonio@i.ua>
2021-12-12 18:51:27 +0200

    fixed a mistake in the name "rating"

--- mental_health/db/migrate/20211203142800_create_ratings.rb
+++ mental_health/db/migrate/20211203142800_create_ratings.rb
@@ -1,6 +1,6 @@
-class CreateRaitings < ActiveRecord::Migration[6.1]
+class CreateRatings < ActiveRecord::Migration[6.1]
   def change
-    create_table :raitings do |t|
+    create_table :ratings do |t|
       t.belongs_to :user
       t.belongs_to :technique
       t.integer :like
