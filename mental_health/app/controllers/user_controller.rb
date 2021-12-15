cad773fb77e45c361122a19e2c674f6770df189c
kvantonio <kvantonio@i.ua>
2021-12-15 16:26:30 +0200

    rate technique

--- mental_health/app/controllers/user_controller.rb
+++ mental_health/app/controllers/user_controller.rb
@@ -1,6 +1,12 @@
 class UserController < ApplicationController
   before_action :check_user_login!
 
+
+  def edit
+    @user = User.find_by_id(session[:user_id])
+    @problems = Problem.all
+  end
+
   def dashboard
     @user = User.find_by_id(session[:user_id])
     @invitation = @user.invitation
@@ -19,10 +25,13 @@ class UserController < ApplicationController
     if @recommendation
       @total_steps = @recommendation.technique.steps.count
       next_step = params[:step_id].to_i + 1
-      if next_step <= @total_steps
+      if next_step < @total_steps
         @recommendation.update(current_step: next_step)
         @recommendation.update(started_at: Time.zone.now) if @recommendation.started_at.nil?
         @step = Step.find_by(number: next_step)
+      else
+        @step = Step.find_by(number: @total_steps)
+        @recommendation.update(current_step: @total_steps)
       end
     end
   end
@@ -40,7 +49,7 @@ class UserController < ApplicationController
   def modal_finish_technique
     user = User.find_by_id(session[:user_id])
     @coach = user.invitation.coach
-    @html_name = 'modal_finish_technique'
+    @html_name = __method__.to_s
     respond_to do |format|
       format.html
       format.js {
@@ -49,15 +58,29 @@ class UserController < ApplicationController
     end
   end
 
+  def rate_technique
+    @user = User.find_by_id(session[:user_id])
+    rating = @user.ratings.find_or_create_by(technique_id: params[:technique_id])
+    if params[:like]
+      rating.update(like: 1, dislike: 0)
+      @user.user_notifications.create(description: 'You like your Technique', status: 1)
+    elsif params[:dislike]
+      rating.update(like: 0, dislike: 1)
+      @user.user_notifications.create(description: 'You dislike your Technique', status: 1)
+    end
+    recommendation = @user.recommendations.find_by(technique_id: params[:technique_id])
+    recommendation.update(finished_at: Time.zone.now) if recommendation.finished_at.nil?
+    redirect_to user_dashboard_path
+  end
+
   def coaches
     @coaches = Coach.all
-
   end
 
   def modal_end_cooperation
     user = User.find_by_id(session[:user_id])
     @coach = user.invitation.coach
-    @html_name = 'modal_end_cooperation'
+    @html_name = __method__.to_s
     respond_to do |format|
       format.html
       format.js {
