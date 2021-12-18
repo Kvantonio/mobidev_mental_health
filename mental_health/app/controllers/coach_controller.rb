356ba7955282cb4b38ac36a8cb8736cfefdefffb
kvantonio <kvantonio@i.ua>
2021-12-18 19:52:50 +0200

    coach dashboard done

--- mental_health/app/controllers/coach_controller.rb
+++ mental_health/app/controllers/coach_controller.rb
@@ -3,9 +3,13 @@ class CoachController < ApplicationController
   def dashboard
     @coach = Coach.find_by_id(session[:coach_id])
     @notifications = @coach.coach_notifications.order('created_at')
-    @invitations = @coach.invitations
-    @techniques = @coach.recommendations.joins(:technique)
-    @techniques_used = @coach.recommendations.joins(:technique).count
+    @invitations = @coach.invitations.where(status: true)
+
+    unique_techniques_id = @coach.recommendations.pluck(:technique_id).uniq
+    unique_techniques = Technique.where(id: unique_techniques_id)
+    @techniques = unique_techniques
+    @techniques_used = unique_techniques_id.count
+    @techniques_with_like = unique_techniques.select { |t| t.ratings.sum(:like).positive? }.count
   end
 
 
