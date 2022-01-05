c0aab0c24ff78f1fb9e9f0d75bc70dd8eac9e28d
kvantonio <kvantonio@i.ua>
2022-01-05 17:58:55 +0200

    change to find_each

--- mental_health/app/workers/hard_worker.rb
+++ mental_health/app/workers/hard_worker.rb
@@ -2,7 +2,7 @@ class HardWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    User.all&.each do |user|
+    User.find_each do |user|
       user.recommendations&.each do |rec|
         if rec.current_step == rec.technique.steps.count && !rec.finished_at
           user.user_notifications.create(description: "Rate and finish your technique #{rec.technique.title}",
